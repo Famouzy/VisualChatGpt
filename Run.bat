cb7894ec5fbeb33a045aa02123149a367249c17c
famouzy <alexutkin636@gmail.com>
2023-07-14 09:29:26 +0300

    Edit

--- Run.bat
+++ Run.bat
@@ -1,12 +1,3 @@
-<<<<<<< Updated upstream
 @echo off
-
 python visual_chatgpt.py --load "Text2Box_cuda:0,Segmenting_cuda:0,Inpainting_cuda:0,ImageCaptioning_cuda:0"
-
-=======
-@echo off
-
-python visual_chatgpt.py --load "Text2Box_cuda:0,Segmenting_cuda:0,Inpainting_cuda:0,ImageCaptioning_cuda:0"
-
->>>>>>> Stashed changes
 pause

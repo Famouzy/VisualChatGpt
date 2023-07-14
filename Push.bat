c3467331303e7bcfe8b8231caf14ed7c19f1bbe1
famouzy <alexutkin636@gmail.com>
2023-07-14 08:50:25 +0300

    Add

--- Push.bat
+++ Push.bat
@@ -1,5 +1,12 @@
+<<<<<<< Updated upstream
 @echo off
 
 git remote add main https://github.com/Famouzy/VisualChatGpt.git
 
+=======
+@echo off
+
+git remote add main https://github.com/Famouzy/VisualChatGpt.git
+
+>>>>>>> Stashed changes
 pause
--- Run.bat
+++ Run.bat
@@ -1,5 +1,12 @@
+<<<<<<< Updated upstream
 @echo off
 
 python visual_chatgpt.py --load "Text2Box_cuda:0,Segmenting_cuda:0,Inpainting_cuda:0,ImageCaptioning_cuda:0"
 
+=======
+@echo off
+
+python visual_chatgpt.py --load "Text2Box_cuda:0,Segmenting_cuda:0,Inpainting_cuda:0,ImageCaptioning_cuda:0"
+
+>>>>>>> Stashed changes
 pause

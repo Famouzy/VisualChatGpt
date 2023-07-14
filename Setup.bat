748d13cf595511b48a31820c1543a6a435000c04
Famouzy <67504634+Famouzy@users.noreply.github.com>
2023-07-14 08:32:08 +0300

    Add files via upload

---
+++ Setup.bat
@@ -0,0 +1,22 @@
+@echo off
+
+git clone https://github.com/microsoft/TaskMatrix.git
+
+# Go to directory
+cd visual-chatgpt
+
+# create a new environment
+conda create -n visgpt python=3.8
+
+# activate the new environment
+conda activate visgpt
+
+#  prepare the basic environments
+pip install -r requirements.txt
+pip install  git+https://github.com/IDEA-Research/GroundingDINO.git
+pip install  git+https://github.com/facebookresearch/segment-anything.git
+
+# prepare your private OpenAI key (for Windows)
+set OPENAI_API_KEY={Your_Private_Openai_Key}
+
+pause
